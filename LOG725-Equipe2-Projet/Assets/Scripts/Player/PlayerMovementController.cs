846362a53b5d7f3ac3ef30d42926d3e322b96ff9
Alexia Gaido--Amoros <alexiagaido--amoros@MacBook-Pro-de-Alexia.local>
2025-03-12 07:21:36 -0400

    ajout échelle (liane grimpable) + modification des mouvements

--- LOG725-Equipe2-Projet/Assets/Scripts/Player/PlayerMovementController.cs
+++ LOG725-Equipe2-Projet/Assets/Scripts/Player/PlayerMovementController.cs
@@ -1,149 +1,164 @@
-
 using System;
-using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEngine.InputSystem.InputAction;
 
 [RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent (typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
 public class PlayerMovementController : MonoBehaviour
 {
-    [Header("     ----- Inputs -----")]
+    [Header("----- Inputs -----")]
     [SerializeField] private InputActionReference MovementInput;
     [SerializeField] private InputActionReference JumpInput;
 
-    private Action<InputAction.CallbackContext> JumpPressedCallback;
-    private Action<InputAction.CallbackContext> JumpReleasedCallback;
+    [Header("----- Movement Parameters -----")]
+    public float JumpStrength = 10.0f;
+    public float WalkSpeed = 100f;
+    public float MaxWalkSpeed = 100f;
 
-    [Header("----- Parameters -----")]
-    public float JumpStrength = 5.0f;
-    public float WalkAcceleration = 30.0f;
-    public float MaxWalkSpeed = 12f;
+    [Header("----- Ladder Parameters -----")]
+    [SerializeField] private float climbSpeed = 10f;
 
     private Rigidbody2D _rigidbody;
     private CapsuleCollider2D _collider;
     private bool facingRight = true;
     private Vector2 _movement = Vector2.zero;
+    private bool isClimbing;
 
-    /// <summary>
-    /// Returns the current direction the Player is trying to move towards.
-    /// </summary>
     public Vector2 GetMoveDirection()
     {
-        return MovementInput.action.ReadValue<Vector2>();
+        Vector2 input = MovementInput.action.ReadValue<Vector2>();
+        Debug.Log($"Input - X: {input.x}, Y: {input.y}"); // Vérifie les inputs
+        return input;
     }
 
-    /// <summary>
-    /// Returns the current direction the Player is facing.
-    /// </summary>
     public Vector2 GetFacingDirection()
     {
         return facingRight ? transform.right : -transform.right;
     }
 
-
-    // Start is called before the first frame update
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _collider = GetComponent<CapsuleCollider2D>();
-
-        JumpPressedCallback = (callbackContext) => OnJumpPressed(callbackContext);
-        JumpReleasedCallback = (callbackContext) => OnJumpReleased(callbackContext);
     }
 
     private void OnEnable()
     {
-        JumpInput.action.performed += JumpPressedCallback;
-        JumpInput.action.canceled += JumpReleasedCallback;
+        JumpInput.action.performed += ctx => OnJumpPressed();
+        JumpInput.action.canceled += ctx => OnJumpReleased();
     }
 
     private void OnDisable()
     {
-        JumpInput.action.performed -= JumpPressedCallback;
-        JumpInput.action.canceled -= JumpReleasedCallback;
+        JumpInput.action.performed -= ctx => OnJumpPressed();
+        JumpInput.action.canceled -= ctx => OnJumpReleased();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        //We get the movement input value, and set the horizontal velocity.
         Vector2 movementInput = GetMoveDirection();
 
-        if (Math.Abs(_rigidbody.velocity.x) <= MaxWalkSpeed)
+        // Mouvement horizontal
+        _movement.x = movementInput.x * WalkSpeed;
+        Debug.Log($"Movement Calculated - X: {_movement.x}, Y: {_movement.y}"); // Vérifie le calcul
+
+        if (isClimbing)
         {
-            _movement = new Vector2(movementInput.x * WalkAcceleration, 0);
+            _movement.y = movementInput.y * climbSpeed;
+            _rigidbody.gravityScale = 0f;
         }
         else
         {
-            _movement = Vector2.zero;
+            _movement.y = 0f;
+            _rigidbody.gravityScale = 1f;
         }
-        
 
-        //We flip the character sprite if we change directions.
-        if (movementInput.x > 0 && !facingRight)
+        // Limiter la vitesse horizontale
+        if (Math.Abs(_rigidbody.velocity.x) > MaxWalkSpeed)
+        {
+            _movement.x = 0f;
+        }
+
+        // Flip
+        if (!isClimbing && movementInput.x > 0 && !facingRight)
         {
             Flip();
         }
-        else if (movementInput.x < 0 && facingRight) {
+        else if (!isClimbing && movementInput.x < 0 && facingRight)
+        {
             Flip();
         }
     }
 
-    /// <summary>
-    /// Returns <c>true</c> if the Player is currently grounded (standing on ground). Returns <c>false</c> otherwise.
-    /// </summary>
     private bool IsGrounded()
     {
-        return Physics2D.CircleCast(transform.position, _collider.size.y, Vector2.down, 1f);
+        bool grounded = Physics2D.CircleCast(transform.position, _collider.size.y / 2, Vector2.down, 1f);
+        Debug.Log($"Is Grounded: {grounded}"); // Vérifie l'état au sol
+        return grounded;
     }
 
-    /// <summary>
-    /// Flips the horizontal transform of the Player in order to flip their sprite.
-    /// </summary>
     private void Flip()
     {
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
-
         facingRight = !facingRight;
     }
 
-
-    private void OnJumpPressed(InputAction.CallbackContext callbackContext)
+    private void OnJumpPressed()
     {
-        if (IsGrounded())
+        if (IsGrounded() && !isClimbing)
         {
+            AddForce(Vector2.up * JumpStrength, ForceMode2D.Impulse, false);
+        }
+        else if (isClimbing)
+        {
+            isClimbing = false;
             AddForce(Vector2.up * JumpStrength, ForceMode2D.Impulse, false);
         }
     }
 
-    /*
-     * We attenuate the vertical velocity to allow the player to control jump height.
-     */
-    private void OnJumpReleased(InputAction.CallbackContext callbackContext)
+    private void OnJumpReleased()
     {
-        if (_rigidbody.velocity.y > 0)
+        if (_rigidbody.velocity.y > 0 && !isClimbing)
         {
             _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y / 2);
         }
     }
 
-    /// <summary>
-    /// Applies a force to the PlayerMovementController's RigidBody.
-    /// </summary>
     public void AddForce(Vector2 force, ForceMode2D forceMode, bool cancelCurrentVelocity)
     {
-        if (cancelCurrentVelocity) {
+        if (cancelCurrentVelocity)
+        {
             _rigidbody.velocity = Vector2.zero;
         }
-        _rigidbody.AddForce(force, ForceMode2D.Impulse);
+        _rigidbody.AddForce(force, forceMode);
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.AddForce(_movement);
+        _rigidbody.velocity = new Vector2(
+            Mathf.Clamp(_movement.x, -MaxWalkSpeed, MaxWalkSpeed),
+            isClimbing ? _movement.y : _rigidbody.velocity.y
+        );
+        Debug.Log($"Velocity Applied - X: {_rigidbody.velocity.x}, Y: {_rigidbody.velocity.y}"); // Vérifie la vélocité
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isClimbing = true;
+            Debug.Log("Entered ladder");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isClimbing = false;
+            Debug.Log("Exited ladder");
+        }
     }
 }
